684fc5f2841968ae767031607f0094bac747d6bb
taemin.hwang <taemin.hwang@lge.com>
2020-04-06 15:07:10 +0900

    Update gprof sample

--- gprof/README.md
+++ gprof/README.md
@@ -1,11 +1,21 @@
+# gprof
+
 ### compile option (-pg)
+```
 g++ sample.cpp -pg
+```
 
 ### run
+```
 ./a.out
+```
 
 ### output
+```
 gmon.out
+```
 
 ### gprof result
-gprof a.out gmon.out
+```
+gprof --graph a.out gmon.out > result.log
+```
--- gprof/sample.cpp
+++ gprof/sample.cpp
@@ -34,8 +34,11 @@ int main() {
     manager_.SetFunc([]() { cout << "hello gprof" << endl;});
     manager_.Run();
     manager_.SetFunc([]() {
-        cout << "hello gprof" << endl;
-        this_thread::sleep_for(1s);
+        int sum = 0;
+        for(int i = 0; i < 10000000; i++) {
+            sum++;
+        }
+        cout << "hello gprof : " << sum << endl;
         });
     manager_.Run();
 }
